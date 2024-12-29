249dd3cd1e8565952920eca00025c7f16b2e0e29
Raí <devvandenberg>
2024-12-29 19:19:00 -0300

    recuperação de projeto

---
+++ agendei-mobile/src/screens/abacalendar/abacalendar.js
@@ -0,0 +1,19 @@
+import { COLORS, FONT_SIZE } from "../../constants/theme.js";
+
+
+export const styles = {
+
+container: {
+    backgroundColor: COLORS.white,
+    flex: 1,
+    padding: 15,
+    
+},
+text: {
+    fontSize: FONT_SIZE.md,
+    color: COLORS.gray1,
+    marginBottom: 15,
+    marginLeft: 10,
+}
+
+}
---
+++ agendei-web/src/pages/regiter/register.jsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+import "./register.css";
+import Logo from "../../assets/logo.png";
+import Fundo from "../../assets/fundo.png";
+
+function Register(){
+    return <div className = "row" >
+        <div className="col-sm-5 d-flex justify-content-center align-items-center text-center ">
+
+            <form className="form-signin">
+                <img src={Logo} className="logo mb-4"  />
+                <h5 className="mb-5">Crie sua conta agora mesmo.</h5>
+                <h5 className="mb-4 text-secondary">Preencha os campos abaixo</h5>
+
+
+                <div className=" mt-4">
+                    <input type="text" placeholder="Nome" 
+                    className=" form-control"/>
+                </div>
+
+                <div className=" mt-2">
+                    <input type="email" placeholder="E-mail" 
+                    className=" form-control"/>
+                </div>
+
+                <div className="mt-2">
+                    <input type="password" placeholder="Senha" 
+                    className="form-control"/>
+                </div>
+
+                <div className="mt-2">
+                    <input type="password" placeholder="Confirme a Senha" 
+                    className="form-control"/>
+                </div>
+
+
+                <div className="mt-3 mb-5">
+                   <button className="btn btn-primary w-100">Criar minha conta</button>
+                </div>
+
+                <div>
+                    <span className="me-1">já tenho uma conta.</span>
+                    <Link to="/">Acessar agora!</Link>
+                </div>
+
+            </form>
+            
+        </div>
+
+        <div className="col-sm-7 d-flex">
+            <img src={Fundo} className="background-login" />
+        </div>
+        
+    </div>
+}
+
+export default Register;
